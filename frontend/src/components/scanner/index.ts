fa7154176ede9dfe261d1e22aafdbaf2b4ce6e1d
Eduard Gavril <a58616296177@gmail.com>
2026-09-14 18:49:45 +0200

    feat: remove scanner components and related hooks, refactor codebase

--- frontend/src/components/scanner/index.ts
+++
@@ -1,6 +0,0 @@
-// Scanner components
-export { default as QRScanner } from './QRScanner'
-export { default as ProductSelector } from './ProductSelector'
-export { default as CartConfirmation } from './CartConfirmation'
-export { default as RewardRedemption } from './RewardRedemption'
-export { default as ScanResult } from './ScanResult'
